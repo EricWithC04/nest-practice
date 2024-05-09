c42f8d0315cad93fb732c782b79e1a6989dec419
EricWithC04 <negativegame117@gmail.com>
2024-05-08 22:34:53 -0300

    feat: method getCarById

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,11 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
 
+    private cars = ['Toyota', 'Honda', 'Jeep'] 
+
     @Get()
     getAllCars() {
-        return ['Toyota', 'Honda', 'Jeep']
+        return this.cars
+    }
+
+    @Get(':id')
+    getCarById( @Param('id') id: string ) {
+        if (+id >= this.cars.length || +id < 0) {
+            return { message: 'Car not found' }
+        } else {
+            return this.cars[+id]
+        }
     }
 
 }
