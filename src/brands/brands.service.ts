f527498e821453afeb60a4f527a66110806359ba
EricWithC04 <negativegame117@gmail.com>
2024-05-09 21:11:46 -0300

    feat: list of brands

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Brand } from './entities/brand.entity';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class BrandsService {
+
+  private brands: Array<Brand> = [
+    {
+      id: uuid(),
+      name: 'Toyota',
+      createdAt: Date.now()
+    },
+    {
+      id: uuid(),
+      name: 'Jeep',
+      createdAt: Date.now()
+    },
+    {
+      id: uuid(),
+      name: 'Honda',
+      createdAt: Date.now()
+    },
+  ]
+  
   create(createBrandDto: CreateBrandDto) {
     return 'This action adds a new brand';
   }
