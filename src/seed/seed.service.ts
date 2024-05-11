387a88248a2ddf0f5de4f187112f3fe1e56afa8b
EricWithC04 <negativegame117@gmail.com>
2024-05-10 22:00:02 -0300

    feat: implemented seed of brands

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -3,17 +3,20 @@ import { CarsService } from 'src/cars/cars.service';
 
 import { CARS_SEED } from './data/cars.seed';
 import { BRANDS_SEED } from './data/brands.seed';
+import { BrandsService } from 'src/brands/brands.service';
 
 @Injectable()
 export class SeedService {
   
   constructor(
-    private readonly carsService: CarsService
+    private readonly carsService: CarsService,
+    private readonly brandsService: BrandsService
   ) {}
 
   populateDB() {
 
     this.carsService.fillCarsWithSeedData( CARS_SEED )
+    this.brandsService.fillBrandsWithSeedData( BRANDS_SEED )
 
     return 'Seeds executed successfully'
   }
